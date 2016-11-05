e56d73a4a6146c0de08a4c034103f4f389c8ade7
luvak4 <luvak4@gmail.com>
2016-11-05 18:13:23 +0100

    Update ad-servopulsante.ino

--- ad-servopulsante/ad-servopulsante.ino
+++ ad-servopulsante/ad-servopulsante.ino
@@ -36,16 +36,16 @@ radio rx ---->|            |-------> radio tx
 */
 #define pin_luceA      2
 #define pin_luceB      3
-#define pin_premiA     4
-#define pin_premiB     5
+#define pin_servoA 4
+#define pin_servoB 5
 #define pin_rx    11
 #define pin_tx    12
 /*--------------------------------
 ** domande (IN)
 */
-#define MASTSa 200 // move servoA (push button)
-#define MASTSb 201 // move servoB (push button)
-#define MASTSa 202 // get stato leds servoA and servoB
+#define MASTSa 200     // move servoA (push button)    (SERVOa)
+#define MASTSb 201     // move servoB (push button)    (SERVOb)
+#define MASTSc 202     // get stato leds servoA and B  (SERVOc)
 /*--------------------------------
 ** risposte (OUT)
 */
@@ -56,14 +56,10 @@ radio rx ---->|            |-------> radio tx
 ** radio tx rx
 */
 byte CIFR[]={223,205,228,240,43,146,241,\
-	     87,213,48,235,131,6,81,26,\
-	     70,34,74,224,27,111,150,22,\
-	     138,239,200,179,222,231,212};
-
-#define mask 0x00FF
-int     INTERIlocali[4]={0,0,0,0}; // N.Mess,Da,Db,Dc
-byte    BYTEradio[BYTEStoTX];
-uint8_t buflen = BYTEStoTX; //for rx
+       87,213,48,235,131,6,81,26,\
+       70,34,74,224,27,111,150,22,\
+       138,239,200,179,222,231,212};
+#define mask       0x00FF
 #define VELOCITAstd   500   // velocita standard
 #define MESSnum         0   // posizione in BYTEradio
 #define DATOa           1   //  "
@@ -71,6 +67,9 @@ uint8_t buflen = BYTEStoTX; //for rx
 #define DATOc           3   //  "
 #define BYTEStoTX       8   // numbero of bytes to tx
 #define AGCdelay 1000       // delay for AGC
+int     INTERIlocali[4]={0,0,0,0}; // N.Mess,DatoA,DatoB,DatoC
+byte    BYTEradio[BYTEStoTX];  // buffer per la trasmissione
+uint8_t buflen = BYTEStoTX; //for rx
 /*--------------------------------
 ** varie
 */
@@ -82,19 +81,12 @@ bool servoBon=false;
 //
 #define SOGLIA   400
 //
-unsigned long tempoA;
-unsigned long tempoB;
-unsigned long tempoC;
-unsigned long tempoD;
-//
 /*////////////////////////////////
 * setup()
 */
 void setup() {
   pinMode(pin_luceA,OUTPUT);
   pinMode(pin_luceB,OUTPUT);
-  pinMode(pin_premiA,INPUT);
-  pinMode(pin_premiB,INPUT);  
   servoA.attach(pin_servoA);
   servoB.attach(pin_servoB);
   Serial.begin(9600); // debug
@@ -103,7 +95,8 @@ void setup() {
   vw_set_rx_pin(pin_rx);
   vw_setup(VELOCITAstd);
   vw_rx_start();    
-  //servoA.write(1000);
+  servoA.write(0);
+  servoB.write(0); 
 }
 //
 /*////////////////////////////////
@@ -111,73 +104,30 @@ void setup() {
 */
 void loop(){
 /*--------------------------------
-** tieni il tempo
-*/
-  // ogni secondo
-  // viene spento lo stato del pin
-  // se led spento
-  if ((abs(millis()-tempoA))>1000){
-    tempoA=millis();
-    if (analogRead(pin_photoA)<SOGLIA){
-      digitalWrite(pin_luceA,LOW);
-    }
-  }
-  if ((abs(millis()-tempoB))>1000){
-    tempoB=millis();
-    if (analogRead(pin_photoB)<SOGLIA){
-      digitalWrite(pin_luceB,LOW);
-    }
-  }
-  if ((abs(millis()-tempoC))>1000){
-    tempoC=millis();
-    if (servoAon){
-      servoAon=false;
-      servoA.write(0);
-    }
-  }
-  if ((abs(millis()-tempoD))>1000){
-    tempoD=millis();
-    if (servoBon){
-      servoBon=false;
-      servoB.write(0);
-    }
-  }    
-  // istantaneamente
-  // accende lo stato del pin se led acceso
-  if (analogRead(pin_photoA)>SOGLIA){
-    digitalWrite(pin_luceA,HIGH);
-    tempoA=millis();      
-  }
-  if (analogRead(pin_photoB)>SOGLIA){
-    digitalWrite(pin_luceB,HIGH);
-    tempoB=millis();
-  }
-  if (digitalRead(pin_premiA)){
-    servoAon=true;
-    servoA.write(90);
-    tempoC=millis();
-  }
-  if (digitalRead(pin_premiB)){
-    servoBon=true;
-    servoB.write(90);
-    tempoD=millis();
-  }
-/*--------------------------------
 ** radio rx
 */
+
+  // 1200 dovrebbe essere piu o meno l'angolo giusto
+ 
+
   if (vw_get_message(BYTEradio, &buflen)){
     vw_rx_stop();
     decodeMessage();
     switch (INTERIlocali[MESSnum]){
     case MASTSa:
-      servoA.write(1000);
+      servoA.write(1200);
+      delay(2000);
+      servoA.write(0);      
       break;
     case MASTSb:
-      servoA.write(1900);
+      servoB.write(1200);
+      delay(2000);
+      servoB.write(0);     
       break;
     case MASTSc:
       break;
-    }    
+    }
+    vw_rx_start(); 
   }
 }
 /*--------------------------------
